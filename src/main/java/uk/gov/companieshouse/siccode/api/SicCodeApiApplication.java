e300721f93c4034a14c555ed42fad836f373fcef
SteveHicksCH <shicks@companieshouse.gov.uk>
2022-02-28 11:41:00 +0000

    renamed bottom level package from siccodeapi to api to be consistent with other projects in CH'

--- src/main/java/uk/gov/companieshouse/siccode/api/SicCodeApiApplication.java
+++ src/main/java/uk/gov/companieshouse/siccode/api/SicCodeApiApplication.java
@@ -1,4 +1,4 @@
-package uk.gov.companieshouse.siccode.siccodeapi;
+package uk.gov.companieshouse.siccode.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
