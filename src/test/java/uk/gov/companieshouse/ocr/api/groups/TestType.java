a0c46ea9db76a0f3f749ced5adf49399a3df5350
SteveHicksCH <shicks@companieshouse.gov.uk>
2022-03-04 12:25:39 +0000

    remove unrequired classes as we now use a Mongo Text Search

--- src/test/java/uk/gov/companieshouse/ocr/api/groups/TestType.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.companieshouse.ocr.api.groups;
-
-public class TestType {
-
-    public static final String UNIT = "unit-test";
-    public static final String INTEGRATION = "integration-test";
-}
