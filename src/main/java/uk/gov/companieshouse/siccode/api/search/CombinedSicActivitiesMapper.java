7c9042194d14d368429d7b7a7bcaf13415221b7a
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-11 11:47:47 +0000

    Update class to interface rather than abstract
    Class does not to be abstract in this case. Abstract class
    implementation of mapstruct mapper is used for more complex uses than
    what is required in our application

--- src/main/java/uk/gov/companieshouse/siccode/api/search/CombinedSicActivitiesMapper.java
+++ src/main/java/uk/gov/companieshouse/siccode/api/search/CombinedSicActivitiesMapper.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public abstract class CombinedSicActivitiesMapper {
+public interface CombinedSicActivitiesMapper {
 
     public abstract List<CombinedSicActivitiesApiModel> storageModelToApiModel(List<CombinedSicActivitiesStorageModel> source);
 
