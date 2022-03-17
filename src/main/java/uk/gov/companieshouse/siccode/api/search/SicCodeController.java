493a848585374c52a68bf4fc6e91a3dccf979a59
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-17 11:33:41 +0000

    Change log message to get error message

--- src/main/java/uk/gov/companieshouse/siccode/api/search/SicCodeController.java
+++ src/main/java/uk/gov/companieshouse/siccode/api/search/SicCodeController.java
@@ -43,7 +43,7 @@ public class SicCodeController {
     @ExceptionHandler(Exception.class)
     public void uncaughtException(Exception e) {
 
-        LOG.error("Something unexpected has occurred: " + e, e);
+        LOG.error("Something unexpected has occurred: " + e.getMessage(), e);
     }
 
 }
