c6d6d7a612a6406a7cbd27056feb087b00985165
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-29 14:42:32 +0100

    Add internalUserInterceptor
    Add internalUserInterceptor to InterceptorConfig
    
    Add required CH api-security-java to pom file
    
    This change will make the api available only when the required eric
    headers are present in a request

--- src/main/java/uk/gov/companieshouse/siccode/api/Logging/InterceptorConfig.java
+++ src/main/java/uk/gov/companieshouse/siccode/api/Logging/InterceptorConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
+import uk.gov.companieshouse.siccode.api.SicCodeApiApplication;
+
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new InternalUserInterceptor(SicCodeApiApplication.APPLICATION_NAME_SPACE));
         registry.addInterceptor(new LoggingInterceptor());
     }
 }
