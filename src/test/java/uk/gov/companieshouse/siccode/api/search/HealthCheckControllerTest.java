5fbd7f30de2782f6ada2a60e9d3aecf658305ca6
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-29 09:14:24 +0100

    Add test to check healthcheck controller

---
+++ src/test/java/uk/gov/companieshouse/siccode/api/search/HealthCheckControllerTest.java
@@ -0,0 +1,32 @@
+package uk.gov.companieshouse.siccode.api.search;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.companieshouse.siccode.api.groups.TestType;
+
+@Tag(TestType.UNIT)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HealthCheckController.class)
+class HealthCheckControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void validateIsHealthy() throws Exception {
+
+    mockMvc.perform(get("/internal/sic-code-search/healthcheck"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString(HealthCheckController.HEALTH_CHECK_MESSAGE)));
+  }
+}
