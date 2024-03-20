45440ee8b6bab3ec42dc199782339854afee166c
JoshFen95 <jfennell@companieshouse.gov.uk>
2024-03-20 13:36:57 +0000

    bi-13605: More ECS Changes
    - add support for multilb listeners
    - set up symbiolic link between userpass and vault.tf
    - update terraform-modules version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.252"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.253"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
@@ -28,7 +28,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.252"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.253"
 
   # Environmental configuration
   environment             = var.environment
@@ -42,6 +42,12 @@ module "ecs-service" {
   lb_listener_arn                   = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority         = local.lb_listener_rule_priority
   lb_listener_paths                 = local.lb_listener_paths
+  multilb_setup                     = true
+  multilb_listeners                 = {
+    "pub-api-lb": {
+      load_balancer_arn             = data.aws_lb.service_lb.arn
+      listener_arn                  = data.aws_lb_listener.service_lb_listener.arn
+    }
   health_check_grace_period_seconds = 240
   healthcheck_healthy_threshold     = "2"
 
@@ -73,6 +79,7 @@ module "ecs-service" {
 
   # Cloudwatch
   cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
+    multilb_cloudwatch_alarms_enabled = var.multilb_cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
   task_environment          = local.task_environment
